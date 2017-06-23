a4b149d38c7f3b6417458b049e9d0c393669cd1a
stefan-evinance <stefan.dimitrov@evinance.com>
2017-06-23 15:11:48 -0400

    Update wait-for-200.sh

--- wait-for-200.sh
+++ wait-for-200.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-echo 'Waiting for HTTP 200 from' $URL
-until $(curl --output /dev/null --silent --head --fail $URL); do
+
+TIMEOUT="${TIMEOUT:-600}"
+seconds=0
+
+echo 'Waiting up to' $TIMEOUT 'seconds for HTTP 200 from' $URL 
+until [ "$seconds" -gt "$TIMEOUT" ] || $(curl --output /dev/null --silent --max-time $TIMEOUT --head --fail $URL); do
   printf '.'
   sleep 5
+  seconds=$((seconds+5))
 done
-echo 'OK'
+
+if [ "$seconds" -lt "$TIMEOUT" ]; then
+  echo 'OK'
+else
+  echo "ERROR: Timed out wating for HTTP 200 from" $URL >&2
+  exit 1
+fi
